841d694a0f40e14565ff18d48c9898b37fe8938e
Rajasimhareddybolla <rajasimhabolla@gmail.com>
2024-09-09 18:54:20 +0530

    rm keys

--- keye.py
+++
@@ -1,4 +0,0 @@
-open_api_key = "sk-kMFgj1gWaT-eCa6AWa8q-qVcsIfARlcQsOBSfTKWH4T3BlbkFJfNIjnxZ70N_OoW9g0zt6NgOye6RzWiPKURtcROQ2sA"
-hf_api_key  = "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"
-gemini  = "AIzaSyCXmjL2XWc2f9Ua8usD0_4Y4G8MM0YVU3U"
-serp_api = "8a477f9b01a1bbf7e5ed8940940dae9d9cff7bc1ce01e868310823d4a724f502"
